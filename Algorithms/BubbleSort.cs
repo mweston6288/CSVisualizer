fc43515954f7ddf8ac6dcbd82e8084bc18b9f755
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-02-22 17:54:59 -0500

    Added BubbleSort

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms
+{
+    class BubbleSort : SortingAlgorithm
+    {
+        public override void sort(int[] array, int size)
+        {
+            int i, j;
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
