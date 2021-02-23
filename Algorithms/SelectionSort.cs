7e8828efd9ed6e255e344c0cf81c9d58fe4f120b
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-02-23 10:48:24 -0500

    Adding SelectionSort file

---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Algorithms
+{
+    class SelectionSort : SortingAlgorithm
+    { 
+    public override void sort(int[] array, int size)
+        {
+            int smallest, i, j;
+
+
+            for (i = 0; i < size; i++)
+            {
+                smallest = i;
+                for (j = i; j < size; j++)
+                {
+                    if (array[j] < array[smallest])
+                        smallest = j;
+                }
+                swap(ref array[i], ref array[smallest]);
+            }
+        }
+    }
+}
