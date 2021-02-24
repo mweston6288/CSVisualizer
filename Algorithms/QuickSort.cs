aee7e6a5703270d6f83c5422f29b3fbf0e03a7a7
felixcloud6288 <mweston6288@knights.ucf.edu>
2021-02-24 11:12:08 -0500

    Added QuickSort code

---
+++ Algorithms/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Algorithms
+{
+    class QuickSort : SortingAlgorithm
+    {
+        public QuickSort(int size) :base(size)
+        {
+        }
+        void quickSort(int low, int high)
+        {
+            if (low < high)
+            {
+                int split = partition(low, high);
+
+                quickSort(low, split - 1);
+                quickSort(split + 1, high);
+            }
+        }
+
+        int partition(int low, int high)
+        {
+            int i, lowPosition;
+            if (low >= high)
+                return low;
+
+            i = array[low];
+            lowPosition = low;
+            low++;
+
+            while (low <= high)
+            {
+                while (low <= high && array[low] <= array[lowPosition])
+                    low++;
+
+                while (high >= low && array[high] > array[lowPosition])
+                    high--;
+                if (low < high)
+                {
+                    swap(ref array[low], ref array[high]);
+                }
+            }
+
+            // Finally we swap the pivot with the point high was pointing to
+            swap(ref array[lowPosition], ref array[high]);
+
+            return high;
+        }
+
+        public override void sort()
+        {
+            quickSort(0, size - 1);
+        }
+    }
+}
